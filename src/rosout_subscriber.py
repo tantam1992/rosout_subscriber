69c5d136b9b5f2973441e9bd1673fbdb643b33a0
JosefGst <tantam1992>
2024-05-23 13:31:05 +0800

    rosout_subscriber

--- src/rosout_subscriber.py
+++ src/rosout_subscriber.py
@@ -6,14 +6,19 @@ from rosgraph_msgs.msg import Log
 class ROSOUTSubscriber:
     def __init__(self):
         self.dwa_failure_count = 0
+        self.global_failure_count = 0
+        rospy.loginfo("rosout_subscriber_node is started.")
         self.sub = rospy.Subscriber('/rosout', Log, self.rosout_callback)
 
     def rosout_callback(self, msg):
         if msg.msg == "DWA planner failed to produce path.":
             self.dwa_failure_count += 1
             rospy.loginfo("DWA planner failure count: %d", self.dwa_failure_count)
+        if msg.msg == "Failed to get a plan.":
+            self.global_failure_count += 1
+            rospy.loginfo("Global planner failure count: %d", self.global_failure_count)
 
-if __:
+if __name__ == "__main__":
     rospy.init_node('rosout_subscriber_node')
     counter = ROSOUTSubscriber()
     rospy.spin()
